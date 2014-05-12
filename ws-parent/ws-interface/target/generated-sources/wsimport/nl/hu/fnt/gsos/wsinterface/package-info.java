6f102516686e92cec57b7158d008bb846ed3e11b
Robin Altena <altenarobin@gmail.com>
2014-05-12 21:57:00 +0200

    .gitignore fix?

--- ws-parent/ws-interface/target/generated-sources/wsimport/nl/hu/fnt/gsos/wsinterface/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.org.prolector/powerservice/request", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package nl.hu.fnt.gsos.wsinterface;
