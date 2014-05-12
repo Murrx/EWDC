a1fb618c288f55692d9e7897a198b60bed7ec8df
Robin Altena <altenarobin@gmail.com>
2014-05-12 21:47:54 +0200

    working call to services

--- ws-parent/ws-consumer/src/main/java/nl/hu/fnt/gsos/wsconsumer/consumer.java
+++ ws-parent/ws-consumer/src/main/java/nl/hu/fnt/gsos/wsconsumer/consumer.java
@@ -1,22 +1,22 @@
-package nl.hu.fnt.gsos.wsconsumer;
-
-import java.math.BigInteger;
-
-import nl.hu.fnt.gsos.wsinterface.Fault_Exception;
-import nl.hu.fnt.gsos.wsinterface.PowerService;
-import nl.hu.fnt.gsos.wsinterface.Request;
-import nl.hu.fnt.gsos.wsinterface.WSInterface;
-
-public class consumer {
-
-	public static void main(String[] args) throws Fault_Exception {
-		PowerService ps = new PowerService();
-		WSInterface wsi = ps.getWSPort();
-		Request r = new Request();
-		r.setPower(new BigInteger("2"));
-		r.setX(new BigInteger("2"));
-		System.out.println(wsi.calculatePower(r));
-		System.out.println("Test");
-	}
-
-}
+//package nl.hu.fnt.gsos.wsconsumer;
+//
+//import java.math.BigInteger;
+//
+//import nl.hu.fnt.gsos.wsinterface.Fault_Exception;
+//import nl.hu.fnt.gsos.wsinterface.PowerService;
+//import nl.hu.fnt.gsos.wsinterface.Request;
+//import nl.hu.fnt.gsos.wsinterface.WSInterface;
+//
+//public class consumer {
+//
+//	public static void main(String[] args) throws Fault_Exception {
+//		PowerService ps = new PowerService();
+//		WSInterface wsi = ps.getWSPort();
+//		Request r = new Request();
+//		r.setPower(new BigInteger("2"));
+//		r.setX(new BigInteger("2"));
+//		System.out.println(wsi.calculatePower(r));
+//		System.out.println("Test");
+//	}
+//
+//}
---
+++ ws-parent/ws-interface/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import nl.hu.fnt.gsos.wsinterface.*;
+public class Main {
+	public static void main(String[]args) throws MalformedURLException{
+//	   	URL wsdlURL = new URL("http://soaptest.parasoft.com/calculator.wsdl");
+//	   	QName serviceName = new QName("http://www.parasoft.com/wsdl/calculator/", "Calculator");
+//	   	Service calcService = Service.create(wsdlURL,serviceName);
+//	   	ICalculator calc = calcService.getPort(ICalculator.class);
+//	   	float result = calc.add(2.0f, 2.0f);
+//	   	System.out.println(result);
+	   	
+	   	URL wsdlURL = new URL("http://www.webservicex.com/globalweather.asmx?WSDL");
+	   	QName serviceName = new QName("http://www.webserviceX.NET", "GlobalWeather");
+	   	Service weatherService = Service.create(wsdlURL, serviceName);
+	   	GlobalWeatherSoap weather = weatherService.getPort(GlobalWeatherSoap.class);
+	   	String result = weather.getWeather("soest", "netherlands");
+	   	System.out.println(result);
+	   	
+	}
+}
